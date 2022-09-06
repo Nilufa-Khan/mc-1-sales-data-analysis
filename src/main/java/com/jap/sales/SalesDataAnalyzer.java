7c2b31c124143b44b8abc5fd65ed481d1fdcf769
Nilufa Khan <khannilufa5@gmail.com>
2022-09-06 14:06:41 +0530

    Test case passed

--- src/main/java/com/jap/sales/SalesDataAnalyzer.java
+++ src/main/java/com/jap/sales/SalesDataAnalyzer.java
@@ -38,20 +38,12 @@ public class SalesDataAnalyzer {
 
 
     public List<SalesRecord> getAllCustomersSortedByPurchaseAmount(List<SalesRecord> salesData){
-       Comparator<SalesRecord> recordComparator = (a1,a2) -> Double.compare (a1.getAmount() , a2.getAmount());
+       Comparator<SalesRecord> recordComparator = (a1,a2) -> (int) (a2.getAmount() - a1.getAmount());
        Collections.sort(salesData,recordComparator);
       return salesData;
     }
     public SalesRecord getTopCustomerWhoSpentMaxTimeOnSite(List<SalesRecord> salesData){
-
-
-
-
-
-
-
-
-        Comparator<SalesRecord> recordComparator = (a1,a2) -> Double.compare(a1.getTime_on_site() , a2.getTime_on_site());
+       Comparator<SalesRecord> recordComparator = (a1,a2) -> (int)(a2.getTime_on_site() - a1.getTime_on_site());
         Collections.sort(salesData,recordComparator);
         return salesData.get(0);
     }
